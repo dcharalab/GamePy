8e65fc13915fd97af77c89814112f6d3caa1de5e
Dimitris Charalampidopoulos <53306847+shikraiho@users.noreply.github.com>
2021-08-08 11:43:26 +0300

    changes for Flake8

--- addone.py
+++ addone.py
@@ -1,69 +1,75 @@
-import random 
+import random
 import keyboard
-import os  
+import os
 
 
 def print_array(array):
-    os.system('cls')  
+    os.system('cls')
     for row in array:
         print(row)
 
-def is_valid_to_proceed(x,y):
+
+def is_valid_to_proceed(x, y):
     if x > 4 or y > 4 or x < 0 or y < 0:
         return False
     else:
         return True
 
+
 def initialize():
     x = random.randrange(5)
     y = random.randrange(5)
-    return x,y
+    return x, y
+
 
 def move_left(array, start_x, start_y, x, y, start_num):
     old_y = start_y
     start_y = start_y - 1
-    if  is_valid_to_proceed(start_x, start_y):
+    if is_valid_to_proceed(start_x, start_y):
         if x == start_x and y == start_y:
-            array[start_x][start_y] =  array[start_x][old_y] + 1
+            array[start_x][start_y] = array[start_x][old_y] + 1
         else:
             array[start_x][start_y] = array[start_x][old_y]
         array[start_x][old_y] = 0
     else:
         start_y = start_y + 1
-    
-    return  start_x, start_y, x, y
+    return start_x, start_y, x, y
+
 
 def move_backwards(array, start_x, start_y, x, y, start_num):
     old_x = start_x
     start_x = start_x + 1
-    if  is_valid_to_proceed(start_x, start_y):
+    if is_valid_to_proceed(start_x, start_y):
         if x == start_x and y == start_y:
-            array[start_x][start_y] =  array[old_x][start_y] +  1
+            array[start_x][start_y] = array[old_x][start_y] + 1
         else:
             array[start_x][start_y] = array[old_x][start_y]
         array[old_x][start_y] = 0
     else:
         start_x = start_x - 1
-    return  start_x, start_y, x, y
+    return start_x, start_y, x, y
+
 
 def move_forward(array, start_x, start_y, x, y, start_num):
     old_x = start_x
     start_x = start_x - 1
-    if  is_valid_to_proceed(start_x, start_y):
+    if is_valid_to_proceed(start_x, start_y):
         if x == start_x and y == start_y:
-            array[start_x][start_y] =  array[old_x ][start_y] +  1
+            array[start_x][start_y] = array[old_x][start_y] + 1
         else:
             array[start_x][start_y] = array[old_x][start_y]
         array[old_x][start_y] = 0
     else:
         start_x = start_x + 1
     return start_x, start_y, x, y
+
+
 def move_right(array, start_x, start_y, x, y, start_num):
     old_y = start_y
     start_y = start_y + 1
-    if  is_valid_to_proceed(start_x, start_y):
+    if is_valid_to_proceed(start_x, start_y):
         if x == start_x and y == start_y:
-            array[start_x][start_y] =  array[start_x][old_y] +  1
+            array[start_x][start_y] = array[start_x][old_y] + 1
         else:
             array[start_x][start_y] = array[start_x][old_y]
         array[start_x][old_y] = 0
@@ -71,38 +77,47 @@ def move_right(array, start_x, start_y, x, y, start_num):
         start_y = start_y - 1
     return start_x, start_y, x, y
 
+
 def play_game():
-    start_x, start_y = (0,0)
-    cols_count,rows_count = (5,5)
+    start_x, start_y = (0, 0)
+    cols_count, rows_count = (5, 5)
     start_num = 1
     array = [[0 for x in range(cols_count)] for x in range(rows_count)]
-  
-    x,y = initialize()
+
+    x, y = initialize()
     array[x][y] = start_num
 
     if x == start_x and y == start_y:
         array[start_x][start_y] = array[x][y] + start_num
-        while x == start_x and y == start_y:  
-            x,y = initialize()
+        while x == start_x and y == start_y:
+            x, y = initialize()
         array[x][y] = start_num
     else:
         array[start_x][start_y] = start_num
 
     while(True):
         print_array(array)
-        
+
         if keyboard.read_key() == "a":
-             start_x,start_y,x,y =  move_left(array, start_x, start_y, x, y, start_num)
+            start_x, start_y, x, y = move_left(array, start_x,
+                                               start_y, x,
+                                               y, start_num)
         elif keyboard.read_key() == "s":
-             start_x,start_y,x,y =  move_backwards(array, start_x, start_y, x, y, start_num)
+            start_x, start_y, x, y = move_backwards(array, start_x,
+                                                    start_y, x,
+                                                    y, start_num)
         elif keyboard.read_key() == "w":
-             start_x,start_y,x,y =  move_forward(array, start_x, start_y, x, y, start_num)
+            start_x, start_y, x, y = move_forward(array, start_x,
+                                                  start_y, x,
+                                                  y, start_num)
         elif keyboard.read_key() == "d":
-             start_x,start_y,x,y =  move_right(array, start_x, start_y, x, y, start_num)
+            start_x, start_y, x, y = move_right(array, start_x,
+                                                start_y, x,
+                                                y, start_num)
 
         if x == start_x and y == start_y:
             while x == start_x and y == start_y:
-                x,y = initialize()
+                x, y = initialize()
             array[x][y] = start_num
 
 
